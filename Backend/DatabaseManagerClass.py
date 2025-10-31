6195c860a412efa6a12dcb405b817e294af275f9
SamDoughtyCodes <samdcodes@gmail.com>
2025-10-31 13:13:14 +0000

    M1 Included docstrings on current methods

--- Backend/DatabaseManagerClass.py
+++ Backend/DatabaseManagerClass.py
@@ -2,11 +2,28 @@ from sqlite3 import connect  # Import library to connect to and alter the databa
 
 # Class used for managing the database
 class database_manager:
-    # Constructor method
-    def __init__(self, file_addr) -> None:
+    def __init__(self, file_addr: str) -> None:
+        """
+        Constructor method called when the object is initialised 
+            
+        Parameters:
+            file_addr (string): The file address of the database
+        """
         self.file = file_addr
         self.cursor = connect(file_addr)
 
-    # Method to create a bespoke database query
-    def fetch_all_records(self, table, fields, *specifier):
-        pass  # Fill this when continuing
+    def fetch_all_records(self, table: str, fields: list[str], *specifier):
+        """
+        Method to create a bespoke database query
+
+        Parameters:
+            table (str): The table from which to fetch records
+            fields (list of strings): The fields to return data from, passed as a list
+            *specifier (list of unknown/varied types): The condition of which to fetch data on, stored as a list including a field and value
+        
+        Returns:
+            data (list of unknown/varied types): The data returned from the database query
+        """
+        fields_str = ""
+        for i in range(len(fields)):  # Iterate over all of the provided fields to return
+            fields_str += str(i)
