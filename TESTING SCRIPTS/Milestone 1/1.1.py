3eaeee95f3aa34893e41bf8190c9bedfc01172d7
SamDoughtyCodes <samdcodes@gmail.com>
2025-11-12 15:18:11 +0000

    Renamed 1.1 and added test 1.2 to this file

--- TESTING SCRIPTS/Milestone 1/1.1.py
+++
@@ -1,6 +0,0 @@
-### SETUP FOR IMPORTS  ###
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))  # Add the parent directory to the system path
-
-### TESTING ###
