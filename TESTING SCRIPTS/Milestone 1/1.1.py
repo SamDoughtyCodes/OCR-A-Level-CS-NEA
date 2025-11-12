6b9d8daa4a7f7df5d30b2459dd6b9a424f72f4af
SamDoughtyCodes <samdcodes@gmail.com>
2025-11-12 14:58:40 +0000

    Updated template and started 1.1 file

--- TESTING SCRIPTS/Milestone 1/1.1.py
+++ TESTING SCRIPTS/Milestone 1/1.1.py
@@ -2,4 +2,5 @@
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))  # Add the parent directory to the system path
-# then import like you would from root directory (OCR-A-LEVEL-CS-NEA)
+
+### TESTING ###
