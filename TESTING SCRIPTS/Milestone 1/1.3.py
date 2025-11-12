4e285cebc9c1389724a321704306372919337bb7
SamDoughtyCodes <samdcodes@gmail.com>
2025-11-12 15:27:58 +0000

    Added tests 1.3 to 1.5, updated template

---
+++ TESTING SCRIPTS/Milestone 1/1.3.py
@@ -0,0 +1,15 @@
+### SETUP FOR IMPORTS  ###
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))  # Add the parent directory to the system path
+
+### TEST 1.3: NEW USER ADDED (INVALID) ###
+from Backend.DatabaseManagerClass import database_manager
+db_manager = database_manager(r"../../site_data.db")
+
+db_manager.create_new_user(
+    "Student",
+    "InvalidUser",
+    "e92c5623a54293eff4a8d60f936cfbe021da0cec711ca5a772508eca486f65e0",
+    "19doughty.s@kesacademy.org.uk"
+)
--- TESTING SCRIPTS/testest.py
+++ TESTING SCRIPTS/testest.py
@@ -1,5 +1,5 @@
 ### SETUP FOR IMPORTS  ###
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))  # Add the parent directory to the system path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))  # Add the parent directory to the system path
 # then import like you would from root directory (OCR-A-LEVEL-CS-NEA)
