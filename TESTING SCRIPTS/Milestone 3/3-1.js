019abaab7653b86e1b4e12177d6453ba7539e160
SamDoughtyCodes <samdcodes@gmail.com>
2026-01-25 20:54:04 +0000

    M3V2 Changes during testing

--- TESTING SCRIPTS/Milestone 3/3-1.js
+++ TESTING SCRIPTS/Milestone 3/3-1.js
@@ -1,3 +1,3 @@
-let id = 0;
+let id = 1;
 let url = "http://localhost:8000/api/stud_data/" + id.toString();
-fetch(url).then(resp => resp.json()).then(r_json => console.log(r_json));
+fetch(url).then(resp => resp.json()).then(r_json => console.log(r_json.personal, r_json.classes, r_json.submissions));
