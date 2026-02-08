5cc489793fe09fbbf64d3de99e98324fe3af9a5b
SamDoughtyCodes <samdcodes@gmail.com>
2026-02-08 12:03:39 +0000

    M4 Created new files for dashboard buttons to redirect to

---
+++ Frontend/Teacher Area/analysis/analysis.js
@@ -0,0 +1,24 @@
+// --- Validate token ---
+let token = localStorage.getItem("token");  // Fetch the token from storage
+// Check if a token was found
+if (token === null) {  // If no token
+    // Redirect to login page
+    window.location = "/Frontend/login/login.html";
+} else {
+    // Make API call to validate found token
+    let call_url = "http://localhost:8000/api/validate";
+    fetch(call_url, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }).then(response => {
+        if (response.status === 403 || response.status === 401) {
+            window.location = "/Frontend/login/login.html";
+        } else {
+            return response.json();  // Return the promise to cast response
+        }
+    }).then(json_resp => {  // Once casted, store the payload in local storage
+        localStorage.setItem("payload", JSON.stringify(json_resp.payload));
+        console.log("Token validated, username: " + json_resp.payload.username);
+    });
+}
