c180e30eb4f4a6e77b9b4060b4b6c590ef82cbbe
SamDoughtyCodes <samdcodes@gmail.com>
2026-03-16 17:50:47 +0000

    M8 Filled placeholders for tasks tab

--- Frontend/Student Area/dashboard/stud_dash_script.js
+++ Frontend/Student Area/dashboard/stud_dash_script.js
@@ -17,7 +17,7 @@ if (token === null) {  // If no token
         } else {
             return response.json();  // Return the promise to cast response
         }
-    }).then(json_resp => {  // Once casted, store the payload in local storage
+    }).then(async json_resp => {  // Once casted, store the payload in local storage
         localStorage.setItem("payload", JSON.stringify(json_resp.payload));
         console.log("Token validated, username: " + json_resp.payload.username);
 
@@ -26,5 +26,28 @@ if (token === null) {  // If no token
         const xp_text = document.getElementById("header_xp_text");
         welc_text.innerHTML = `Welcome, ${json_resp.payload.username}`;
         xp_text.innerHTML = `${String(json_resp.payload.xp)}XP`;
+
+        let all_incomplete = [];
+
+        // Get the active tasks for the student
+        let active = await fetch(`http://localhost:8000/api/students/active/${json_resp.payload.username}`);
+        let json_active = await active.json();
+        let number_active = json_active.length;
+        json_active.forEach(element => {all_incomplete.push(element);});
+
+        // Get the inactive tasks for the student
+        let inactive = await fetch(`http://localhost:8000/api/students/past/${json_resp.payload.username}`);
+        let json_inactive = await inactive.json();
+        let number_inactive = json_inactive.length;
+        json_inactive.forEach(element => {all_incomplete.push(element);});
+
+        // Update placeholders
+        const active_tasks = document.getElementById("tt_active");
+        active_tasks.innerHTML = `${String(number_active)}<br>Active Tasks`;
+        const overdue_tasks = document.getElementById("tt_overdue");
+        overdue_tasks.innerHTML = `${String(number_inactive)}<br>Overdue Tasks`;
+
+        //TODO: Add code to distinguish between overdue and past tasks,
+        //      this can literally just be checking against the student's submissions
     });
 }
